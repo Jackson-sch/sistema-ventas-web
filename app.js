3629ba4ae032ec77f8b477f5a1b527441ce7d825
Jackson-sch <darwinjackson.12@gmail.com>
2023-01-20 09:55:35 -0500

    Version 0.0.03

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
-require('dotenv').config()
+const { timeStamp } = require("console");
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config();
+const app = express();
 
-const express = require('express')
+/* Connecting to the MongoDB database. */
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.MONGO_DB_PASS}@desarrollo.vge1rqc.mongodb.net/sistema-web?retryWrites=true&w=majority`
+  )
+  .then((result) => {
+    app.listen(PORT, () => {
+      console.log(`servidor en el puerto ${PORT}`);
+    });
+    console.log("conexion exitosa a la base de datos mongodb");
+  })
+  .catch((err) => console.log(err));
 
-const app = express()
+/* Creating a new schema for the productos collection. */
+const productosSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true
+  },
+  precio: Number,
+  },
+  { timeStamp: true }
+  );
+  
+const Producto = mongoose.model('Producto', productosSchema)
 
-app.get('/', (req, res) => {
-  console.log('Petición recibida')
+/* A middleware that parses the body of the request and makes it available in the req.body property. */
+app.use(express.json())
 
-  res.send('<h1>holaa mundo con nodemon </h1> bienvenido empezamos')
-})
+/* A route that is listening for a post request. */
+app.post("/api/v1/productos", (req, res) => {
+  /* Create a new Producto object. */
+  const producto = new Producto(req.body)
+  /* Save the new producto to the database. */
+  producto.save().then((result) => {
+    res.status(201).json({ ok: true })
+  })
+  .catch((err) => console.log(err))
+  
+});
 
-const PORT = process.env.PORT || 4000
+/* Serving the static files in the public folder. */
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-  console.log(`servidor en el puerto ${PORT}`)
-})
+/* Setting the port to 4000 if the environment variable PORT is not set. */
+const PORT = process.env.PORT || 4000;
